e1572f3c190ec2488923412798bbcdae95e94a83
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2014-10-28 02:55:01 -0300

    fix(ast03 Main): remove example 3, change format, show matching table

--- ast03/q1/src/Main.java
+++ ast03/q1/src/Main.java
@@ -6,8 +6,9 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
-        // testfile one
+        /*
         Scanner testfile = new Scanner(new File("testfile"));
+
         NFA nfa = new NFA(testfile);
         DFA dfa = nfa.convertToDFA();
 
@@ -15,12 +16,13 @@ public class Main {
             System.out.println("State: " + s);
             System.out.println("\tEPSILON-Closure: " + nfa.getEpsilonClosure(s));
         }
-        System.out.println(dfa.getTable());
+        System.out.println(dfa.getTable(7));
+        System.out.println(dfa.getMatchingTable(15));
         System.out.println(dfa.getGrail());
+        */
 
-
-        // testfile two
         Scanner testfile_2 = new Scanner(new File("testfile_2"));
+
         NFA nfa_2 = new NFA(testfile_2);
         DFA dfa_2 = nfa_2.convertToDFA();
 
@@ -28,20 +30,8 @@ public class Main {
             System.out.println("State: " + s);
             System.out.println("\tEPSILON-Closure: " + nfa_2.getEpsilonClosure(s));
         }
-        System.out.println(dfa_2.getTable());
+        System.out.println(dfa_2.getTable(8));
+        System.out.println(dfa_2.getMatchingTable(33));
         System.out.println(dfa_2.getGrail());
-
-
-        // testfile three
-        Scanner testfile_3 = new Scanner(new File("testfile_3"));
-        NFA nfa_3 = new NFA(testfile_3);
-        DFA dfa_3 = nfa_3.convertToDFA();
-
-        for (State s : nfa_3.getStates()) {
-            System.out.println("State: " + s);
-            System.out.println("\tEPSILON-Closure: " + nfa_3.getEpsilonClosure(s));
-        }
-        System.out.println(dfa_3.getTable());
-        System.out.println(dfa_3.getGrail());
     }
 }
